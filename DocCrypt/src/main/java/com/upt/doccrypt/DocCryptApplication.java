ee6506a33f123899ff135841e87ef340be8ae9af
IonToncu <75910517+IonToncu@users.noreply.github.com>
2022-11-16 22:55:18 +0200

    First backend commit

---
+++ DocCrypt/src/main/java/com/upt/doccrypt/DocCryptApplication.java
@@ -0,0 +1,13 @@
+package com.upt.doccrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocCryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocCryptApplication.class, args);
+    }
+
+}
