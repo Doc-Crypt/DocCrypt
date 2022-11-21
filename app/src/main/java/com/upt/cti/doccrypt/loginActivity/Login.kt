faeb4f1c6ea160914f04cc0b335c35ab93c8a164
IonToncu <75910517+IonToncu@users.noreply.github.com>
2022-11-22 01:32:31 +0200

    added Sign in/ Sign up functionality

--- app/src/main/java/com/upt/cti/doccrypt/loginActivity/Login.kt
+++ app/src/main/java/com/upt/cti/doccrypt/loginActivity/Login.kt
@@ -1,12 +1,45 @@
 package com.upt.cti.doccrypt.loginActivity
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.upt.cti.doccrypt.R
 
 class Login : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        findViewById<Button>(R.id.login_button).setOnClickListener {
+            login()
+        }
+
+        findViewById<Button>(R.id.register_button).setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun login(){
+        val email = findViewById<EditText>(R.id.login_email).text.toString()
+        val password = findViewById<EditText>(R.id.login_password).text.toString()
+        Log.e(TAG, email)
+        Log.e(TAG, password)
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Successfully LoggedIn", Toast.LENGTH_SHORT).show()
+            } else
+                Toast.makeText(this, "Log In failed ", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/upt/cti/doccrypt/loginActivity/Register.kt
+++ app/src/main/java/com/upt/cti/doccrypt/loginActivity/Register.kt
@@ -1,12 +1,63 @@
 package com.upt.cti.doccrypt.loginActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import com.upt.cti.doccrypt.R
 
 class Register : AppCompatActivity() {
+
+    lateinit var etEmail: EditText
+    private lateinit var etPass: EditText
+    private lateinit var btnSignUp: Button
+    lateinit var tvRedirectLogin: TextView
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        etEmail = findViewById(R.id.register_email)
+        etPass = findViewById(R.id.register_password)
+        btnSignUp = findViewById(R.id.create_account_button)
+        tvRedirectLogin = findViewById(R.id.sign_in_button)
+
+        // Initialising auth object
+        auth = FirebaseAuth.getInstance()
+
+        btnSignUp.setOnClickListener {
+            signUpUser()
+        }
+
+        tvRedirectLogin.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun signUpUser() {
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+
+        if (email.isBlank() || pass.isBlank()) {
+            Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Successfully Singed Up", Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(this, "Singed Up Failed!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
