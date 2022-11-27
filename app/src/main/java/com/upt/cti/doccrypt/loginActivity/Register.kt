59f78befdbcbba3d303e1bd13fc546baaeef3636
vlahdu <69470698+vlahdu@users.noreply.github.com>
2022-11-27 17:12:22 +0200

    authentification design finished

--- app/src/main/java/com/upt/cti/doccrypt/loginActivity/Register.kt
+++ app/src/main/java/com/upt/cti/doccrypt/loginActivity/Register.kt
@@ -1,22 +1,21 @@
 package com.upt.cti.doccrypt.loginActivity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
 import com.upt.cti.doccrypt.R
 
 class Register : AppCompatActivity() {
 
-    lateinit var etEmail: EditText
-    private lateinit var etPass: EditText
-    private lateinit var btnSignUp: Button
-    lateinit var tvRedirectLogin: TextView
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var visibilityButton : ImageButton
+    private lateinit var userRadioButton: RadioButton
+    private lateinit var notaryRadioButton: RadioButton
 
     private lateinit var auth: FirebaseAuth
 
@@ -24,31 +23,50 @@ class Register : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        etEmail = findViewById(R.id.register_email)
-        etPass = findViewById(R.id.register_password)
-        btnSignUp = findViewById(R.id.create_account_button)
-        tvRedirectLogin = findViewById(R.id.sign_in_button)
-
         // Initialising auth object
         auth = FirebaseAuth.getInstance()
 
-        btnSignUp.setOnClickListener {
+        visibilityButton = findViewById(R.id.register_password_visibility)
+        email = findViewById(R.id.register_email)
+        password = findViewById(R.id.register_password)
+
+        visibilityButton.setOnClickListener {
+            if (password.transformationMethod.equals(HideReturnsTransformationMethod.getInstance())) {
+                password.transformationMethod = PasswordTransformationMethod.getInstance()
+                visibilityButton.setImageResource(R.drawable.eye_show)
+            } else {
+                password.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                visibilityButton.setImageResource(R.drawable.eye_hide)
+            }
+        }
+
+        findViewById<Button>(R.id.create_account_button).setOnClickListener {
             signUpUser()
         }
 
-        tvRedirectLogin.setOnClickListener {
+        findViewById<TextView>(R.id.sign_in_button).setOnClickListener {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
     }
 
     private fun signUpUser() {
-        val email = etEmail.text.toString()
-        val pass = etPass.text.toString()
+        val email = email.text.toString()
+        val pass = password.text.toString()
 
         if (email.isBlank() || pass.isBlank()) {
             Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
             return
+        }
+
+        userRadioButton = findViewById(R.id.type_user)
+        notaryRadioButton = findViewById(R.id.type_notary)
+
+        userRadioButton.setOnCheckedChangeListener { _, isChecked ->
+            run {
+                if (isChecked)
+                    notaryRadioButton.isChecked = false
+            }
         }
 
         auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
